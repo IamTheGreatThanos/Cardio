4bfa2e1bd3fdea6b36936f429a2e8fb9370fc7f5
akniet18 <akniet.akniet2016@mail.ru>
2020-12-09 12:44:44 +0600

    first

--- utils/mysocket.py
+++ utils/mysocket.py
@@ -1,30 +1,49 @@
-import socket
-import sys
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect the socket to the port where the server is listening
-server_address = ('157.230.91.217', 10000)
-print(sys.stderr, 'connecting to %s port %s' % server_address)
-sock.connect(server_address)
+from tornado.ioloop import IOLoop
+from tornado.tcpserver import TCPServer
 
 
-try:
-    # Send data
-    message = 'This is the message.  It will be repeated.'
-    print(sys.stderr, 'sending "%s"' % message)
-    sock.sendall(message)
+class MyTCPServer(TCPServer):
+    def handle_stream(self, stream, address):
+        def got_data(data):
+            print("Input: {}".format(repr(data)))
+            stream.write("OK", stream.close)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
+        stream.read_until("\n", got_data)
 
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print(sys.stderr, 'received "%s"' % data)
 
-finally:
-    print(sys.stderr, 'closing socket')
-    sock.close()
+if __name__ == '__main__':
+    server = MyTCPServer()
+    server.listen(9876)
+    IOLoop.instance().start()
+
+# import socket
+# import sys
+
+# # Create a TCP/IP socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# # Connect the socket to the port where the server is listening
+# # server_address = ('157.230.91.217', 10000)
+# server_address = ('192.168.1.101', 10000)
+# print(sys.stderr, 'connecting to %s port %s' % server_address)
+# sock.connect(server_address)
+
+
+# try:
+#     # Send data
+#     message = 'This is the message.  It will be repeated.'
+#     print(sys.stderr, 'sending "%s"' % message)
+#     sock.sendall(message)
+
+#     # Look for the response
+#     amount_received = 0
+#     amount_expected = len(message)
+
+#     while amount_received < amount_expected:
+#         data = sock.recv(16)
+#         amount_received += len(data)
+#         print(sys.stderr, 'received "%s"' % data)
+
+# finally:
+#     print(sys.stderr, 'closing socket')
+#     sock.close()
