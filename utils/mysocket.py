e56d080d7be0bfd772b8686c32b7c2a7ed222982
akniet18 <akniet.akniet2016@mail.ru>
2020-12-09 12:17:57 +0600

    first

--- utils/mysocket.py
+++ utils/mysocket.py
@@ -1,10 +1,30 @@
 import socket
+import sys
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    host = "back.cardioservice.com.kz"
-    port = 13
+# Connect the socket to the port where the server is listening
+server_address = ('157.230.91.217', 10000)
+print(sys.stderr, 'connecting to %s port %s' % server_address)
+sock.connect(server_address)
 
-    s.connect((host, port))
-    s.sendall(b'')
-    print(str(s.recv(4096), 'utf-8'))
+
+try:
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print(sys.stderr, 'sending "%s"' % message)
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print(sys.stderr, 'received "%s"' % data)
+
+finally:
+    print(sys.stderr, 'closing socket')
+    sock.close()
