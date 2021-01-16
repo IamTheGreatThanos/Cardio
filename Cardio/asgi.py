366997756e6e93076e4816f4b25759eaab9cd8da
akniet18 <akniet.akniet2016@mail.ru>
2021-01-17 01:02:18 +0600

    first

--- Cardio/asgi.py
+++ Cardio/asgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 import django
+from channels.routing import get_default_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cardio.settings')
 django.setup()
