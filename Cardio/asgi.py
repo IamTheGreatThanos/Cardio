2aaaa367cc0a8154fca4ff3a45825722719a7229
akniet18 <akniet.akniet2016@mail.ru>
2021-01-17 01:01:33 +0600

    first

--- Cardio/asgi.py
+++ Cardio/asgi.py
@@ -9,11 +9,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 import django
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.conf.urls import url
-from CardioApp.consumers import PointerConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cardio.settings')
 django.setup()
