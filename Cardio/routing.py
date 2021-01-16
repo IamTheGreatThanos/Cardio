54f4d9d8f207ce7abbe766857c227cabcba599ef
akniet18 <akniet.akniet2016@mail.ru>
2021-01-17 01:08:58 +0600

    first

--- Cardio/routing.py
+++ Cardio/routing.py
@@ -1,14 +1,15 @@
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.conf.urls import url
+from django.urls import path, include
+
 from CardioApp.consumers import PointerConsumer
 
 application = ProtocolTypeRouter({
     # Just HTTP for now. (We can add other protocols later.)
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            url(r"^chart/$", PointerConsumer.as_asgi()),
+            path("chart/", PointerConsumer),
         ])
     ),
 })
