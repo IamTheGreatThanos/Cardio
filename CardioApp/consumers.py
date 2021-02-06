41f37f627c257090fc72af4bbf081e5d8694a450
akniet18 <akniet.akniet2016@mail.ru>
2021-02-06 16:45:27 +0600

    first

--- CardioApp/consumers.py
+++ CardioApp/consumers.py
@@ -10,8 +10,9 @@ class PointerConsumer(JsonWebsocketConsumer):
     def connect(self):
         # self.group_name = "room_"+str(self.scope['wid'])
         # self.group_name = "room_"+str(self.scope["url_route"]["kwargs"]["wid"])
-        self.group_name = urllib.parse.parse_qs(self.scope['query_string'].decode('utf8'))
-        print(self.group_name)
+        params = urllib.parse.parse_qs(self.scope['query_string'].decode('utf8'))
+        self.group_name = "user"
+        print(params)
         async_to_sync(self.channel_layer.group_add)(
             self.group_name,
             self.channel_name
